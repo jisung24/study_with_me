68107b9b1a5098e85511546a67e46ef4e0b0be3e
jeeeee_sung <85999976+jisung24@users.noreply.github.com>
2023-01-23 01:27:15 +0900

    객체&배열 구조분해할당

---
+++ Algorithm/백준/기본문제/01.입출력과사칙연산/01A+B.js
@@ -0,0 +1,11 @@
+'use strict';
+const [num1, num2] = require('fs').readFileSync('test.txt').toString().split(' ')
+.map((value) => +value);
+// 구조분해할당! 
+// -> 객체 or 배열이라면 무조건 쓸 수 있다. 
+// -> 그래서 만약 split으로 배열로 바꾼 순간!!! => 구조분해할당을 쓸 수 있게 되는거야!
+console.log(num1 + num2);
+
+
+// 정리 
+// 구조분해할당, spread연산자! 
