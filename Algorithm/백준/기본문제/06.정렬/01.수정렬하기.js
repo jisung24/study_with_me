5c093f033425d28b05c910fd31bfb6ef6cbc914e
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-02-18 20:34:36 +0900

    백준 배열,문자열

--- Algorithm/백준/기본문제/06.정렬/01.수정렬하기.js
+++ Algorithm/백준/기본문제/06.정렬/01.수정렬하기.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+let arr = [];
 
-input.shift(); // 제거
-
-input.sort((a, b) => a - b); // 나머지 정렬!
-
-for (let i = 0; i < input.length; i++) {
-  console.log(input[i]);
+// O(N)
+for (let i = 1; i < input.length; i++) {
+  arr.push(Number(input[i]));
 }
+
+// O(N log N)
+let incSort = arr.sort((a, b) => a - b).join("\n");
+console.log(incSort);
+
+// 결국 합쳐도 N^2이 되지않음.
--- Algorithm/백준/기본문제/06.정렬/02.대표값.js
+++ Algorithm/백준/기본문제/06.정렬/02.대표값.js
@@ -1,18 +1,16 @@
 "use strict";
-const values = require("fs")
+// 정렬하고 중앙값을 구하자..!
+const arr = require("fs")
   .readFileSync("dev/stdin")
   .toString()
   .trim()
   .split("\n")
-  .map(Number);
+  .map(Number)
+  .sort((a, b) => a - b);
 
-let representativeValue = (arrList) => {
-  let copyArr = [...arrList];
-  let avg = parseInt(arrList.reduce((acc, cur) => acc + cur) / arrList.length);
-  let centerValues = copyArr.sort((a, b) => a - b)[
-    Math.floor(copyArr.length / 2)
-  ];
-
-  return avg + "\n" + centerValues;
-};
-console.log(representativeValue(values));
+const copyArr = [...arr];
+// 평균도 자연수로...!
+let avg = parseInt(copyArr.reduce((acc, cur) => acc + cur) / copyArr.length);
+let centerValue = copyArr[Math.floor(copyArr.length / 2)];
+console.log(avg);
+console.log(centerValue);
