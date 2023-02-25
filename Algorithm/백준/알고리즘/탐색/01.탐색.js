1b6a0cf5a94037a7cfc60dfa38f0e7f229030ab3
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-02-25 17:45:50 +0900

    0단계 옹알이 + 순위문제

--- Algorithm/백준/알고리즘/탐색/01.탐색.js
+++ Algorithm/백준/알고리즘/탐색/01.탐색.js
@@ -2,8 +2,43 @@
 // dfs : 깊이 우선 탐색
 // bfs : 너비 우선 탐색
 
-// 각 연결된 노드들을 적어준다.
+// bfs같은 경우엔 queue로 구해준다...!
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+  // enqueue
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+
+  // dequeue
+  dequeue() {
+    if (this.size() === 0) {
+      return -1;
+    } else {
+      let value = this.queue[this.front];
+      delete this.queue[this.front];
+      this.front += 1; // front를 1증가시킨다..!
+      return value;
+    }
+  }
+
+  // peek : 큐에서 가장 앞에 있는 수 return!
+  peek() {
+    return this.queue[this.front];
+  }
+}
+
 const graph = {
+  // 각 노드가 property
+  // 각 노드와 연결 돼 있는 노드들을 value로 설정함.
   A: ["B", "C"],
   B: ["A", "D"],
   C: ["A", "E"],
@@ -22,19 +57,10 @@ const graph = {
   P: ["M"],
 };
 
-const bfs = (graph, start) => {
-  const checked = []; // check가 된 노드
-  const willCheck = []; // 아직 check를 못 한 node
+let queue = new Queue();
 
-  willCheck.push(start);
-
-  while (willCheck.length !== 0) {
-    const node = willCheck.shift(); // 큐(First In First Out)
-    if (!checked.includes(node)) {
-      checked.push(node);
-      willCheck.push(...graph[node]);
-    }
-  }
-  return checked;
-};
-console.log(bfs(graph, "A"));
+let arr = [1];
+let i = 0;
+while (arr.length > 0) {
+  // 중간에 0이 돼도 끝에서만 0이 안 되면 계속 반복이 된다.!!!
+}
