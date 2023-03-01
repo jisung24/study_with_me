f97587664ce379e8745383692bff2d22cc811c5f
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-03-01 21:44:10 +0900

    queue

--- Algorithm/프로그래머스/고득점키트/01.정렬/01.k번째 수.js
+++ Algorithm/프로그래머스/고득점키트/01.정렬/01.k번째 수.js
@@ -1,10 +1,5 @@
 "use strict";
-
-// 2번쨰 숫자에서 j번째 숫자까지
-// i + 1,   j + 1
-// 2번쨰 부터 5번쨰
-// index 1부터 4번까지
-// i - 1부터  j까지 slice
+// slice i -1부터 j까지..해서 return
 let array = [1, 5, 2, 6, 3, 7, 4];
 let command = [
   [2, 5, 3],
@@ -12,18 +7,12 @@ let command = [
   [1, 7, 3],
 ];
 let answer = [];
-let solution = (array, command) => {
-  command.forEach((value) => {
-    let slicedArr = array.slice(value[0] - 1, value[1]).sort((a, b) => a - b);
-    answer.push(slicedArr[value[2] - 1]);
-  });
+for (let value of command) {
+  // console.log(value); 2 5 3
+  // 어차피 slice는 원본 훼손을 하지 않아서 마음대로 써도 된다.!!
+  let [first, end, approach] = value;
+  let slicedArr = array.slice(first - 1, end).sort((a, b) => a - b);
+  answer.push(slicedArr[approach - 1]);
+}
 
-  return answer;
-};
-
-console.log(solution(array, command));
-
-// array에 연산을 총 command의 길이만큼 연산을 먹일거지?
-// 그럼 command의 개수 만큼 반복문을 돌려야 돼....
-// array만큼 반복문 돌리고 이러지마..
-// 꼭 필요한 만큼 만!!!
+console.log(answer);
