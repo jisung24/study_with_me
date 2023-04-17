626aa4fc724cb42cbc290d576aed25a912d646e3
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-04-17 19:47:56 +0900

    programmers level 2

--- graph.js
+++ graph.js
@@ -1,7 +1,20 @@
 "use strict";
-// 그래프
-// 정리 방법
-// => 보통 간선과 노선으로 나타낸다.
-// => 길과 지역으로 나타낸다.
 
-// 그래
+let str = "110010101001";
+
+let strArr = str.split(""); // map굳이 해 줄 필요가 없어...!
+let zero = 0;
+let count = 0;
+
+while (strArr.length !== 1 || strArr[0] !== "1") {
+  // 아니라면...!
+  count += 1; // 1번 시도됨
+  zero += strArr.length - strArr.filter((e) => e === "1").length;
+
+  strArr = strArr
+    .filter((e) => e === "1")
+    .length.toString(2)
+    .split("");
+}
+
+console.log([count, zero]);
