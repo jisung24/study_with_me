0a224ca603f46b6ba6c73a9881f23a31739fb122
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-05-03 15:50:57 +0900

    programmers level 1 practice

--- Algorithm/프로그래머스/LV1/07.삼총사.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let number = [-3, -2, -1, 0, 1, 2, 3];
-
-for (let i = 0; i < number.length; i++) {
-  for (let j = i + 1; j < number.length; j++) {}
-}
--- Algorithm/프로그래머스/LV1/tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-console.log(`이번 달 >> ${month}`);
-  console.log(`날짜 >> ${dayCount}`);
