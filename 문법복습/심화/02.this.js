7981183ab8b012b6ac42c777e1ccd3767dc1f89d
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-05-17 15:24:30 +0900

    javascript basis

--- 문법복습/심화/02.this.js
+++ 문법복습/심화/02.this.js
@@ -1,20 +1,17 @@
 "use strict";
-// javascript this
 
-// 누가 말을 하냐에 따라서 "내"라는 단어의 의미가 달라진다.
-// 지성 => 내가 => 지성을 의미함
-// 다른사람 => 내가 => 다른사람을 의미함.
+// this
+// => 함수 호출방식에 의해서 결정된다
+// ❗️javascript함수는 호출이 될 때 인자 이외에, argument객체와 this라는 빈 객체를 암묵적으로 전달받는다.
+// this에 바인딩 할 객체가 정적이 아니라 동적으로 할당이 된다. => 즉, 함수가 호출되기 전까진 모름.. => 어떻게 호출되느냐에 따라서 할당되는 this가 다름.
+
 {
-  const car = {
-    name: "KIA",
-    getName: function () {
-      console.log("car >> ", this); // 이 객체를 의미한다.
-      // 즉, 함수를 누가 호출하는지!
-    },
-  };
+  function test() {
+    return this; // window객체!
+  }
 
-  car.getName();
-
-  const gCar = car.getName;
-  console.log(gCar()); // window
+  console.log(test());
 }
+
+// window : javascript로 브라우져를 제어할 수 있도록 브라우져 전체를 의미하는 객체이다.
+// javascript로 제어하기 위해서 window.~~ 방식으로 접근한다.
